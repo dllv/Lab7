f1448f3e56c9f827bd8343c02f559b179b93dae8
dllv <dllv926@gmail.com>
2020-12-27 15:45:55 +0300

    Добавьте файлы проекта.

---
+++ Files/MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Files
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.tbInfo = new System.Windows.Forms.TextBox();
+            this.tbEdit = new System.Windows.Forms.TextBox();
+            this.lblEdit = new System.Windows.Forms.Label();
+            this.btnGetInfo = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.tbInput.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.tbInput.Location = new System.Drawing.Point(13, 12);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(100, 20);
+            this.tbInput.TabIndex = 0;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(10, 52);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(117, 13);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "Информация о файле";
+            // 
+            // tbInfo
+            // 
+            this.tbInfo.Location = new System.Drawing.Point(13, 68);
+            this.tbInfo.Multiline = true;
+            this.tbInfo.Name = "tbInfo";
+            this.tbInfo.ReadOnly = true;
+            this.tbInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbInfo.Size = new System.Drawing.Size(340, 132);
+            this.tbInfo.TabIndex = 2;
+            // 
+            // tbEdit
+            // 
+            this.tbEdit.Location = new System.Drawing.Point(13, 219);
+            this.tbEdit.Multiline = true;
+            this.tbEdit.Name = "tbEdit";
+            this.tbEdit.ReadOnly = true;
+            this.tbEdit.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbEdit.Size = new System.Drawing.Size(340, 170);
+            this.tbEdit.TabIndex = 3;
+            this.tbEdit.WordWrap = false;
+            // 
+            // lblEdit
+            // 
+            this.lblEdit.AutoSize = true;
+            this.lblEdit.Location = new System.Drawing.Point(10, 203);
+            this.lblEdit.Name = "lblEdit";
+            this.lblEdit.Size = new System.Drawing.Size(107, 13);
+            this.lblEdit.TabIndex = 4;
+            this.lblEdit.Text = "Содержимое файла";
+            // 
+            // btnGetInfo
+            // 
+            this.btnGetInfo.Location = new System.Drawing.Point(213, 6);
+            this.btnGetInfo.Name = "btnGetInfo";
+            this.btnGetInfo.Size = new System.Drawing.Size(140, 30);
+            this.btnGetInfo.TabIndex = 5;
+            this.btnGetInfo.Text = "Загрузить информацию";
+            this.btnGetInfo.UseVisualStyleBackColor = true;
+            this.btnGetInfo.Click += new System.EventHandler(this.btnGetInfo_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(213, 395);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(140, 30);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Сохранить изменения";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 433);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnGetInfo);
+            this.Controls.Add(this.lblEdit);
+            this.Controls.Add(this.tbEdit);
+            this.Controls.Add(this.tbInfo);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.tbInput);
+            this.Name = "MainForm";
+            this.Text = "Просмотр информации о файлах";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.TextBox tbInfo;
+        private System.Windows.Forms.TextBox tbEdit;
+        private System.Windows.Forms.Label lblEdit;
+        private System.Windows.Forms.Button btnGetInfo;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Files/MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Files
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private FileInfo fiSource;
+        private string fileName;
+        private FileStream fsSource;
+        private StreamReader srSource;
+        private StreamWriter swSource;
+
+        private void btnGetInfo_Click(object sender, EventArgs e)
+        {
+            fileName = tbInput.Text;
+            fiSource = new FileInfo(fileName);
+
+            tbInfo.Clear();
+            tbEdit.Clear();
+            tbInput.Clear();
+            try
+            {
+                if (fiSource.Exists)
+                {
+                    lblFileName.Text = "Информация о файле:" + fileName;
+                    tbInfo.Text += "Время создания файла: " + fiSource.CreationTime.ToString() + "\r\n";
+                    tbInfo.Text += "Размер файла " + fiSource.Length + " байт \r\n";
+                    tbInfo.Text += "Полный путь к файлу: " + fiSource.FullName.ToString() + "\r\n";
+
+                    if (Path.GetExtension(fileName) == ".txt")
+                    {
+                        btnSave.Enabled = true;
+                        tbEdit.ReadOnly = false;
+
+                        fsSource = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                        srSource = new StreamReader(fsSource);
+                        string line;
+                        line = srSource.ReadLine();
+                        while (line != null)
+                        {
+                            tbEdit.Text += line + "\r\n";
+                            line = srSource.ReadLine();
+                        }
+                        srSource.Close();
+                    }
+                    else
+                    {
+                        tbEdit.Text += "Файл не является текстовым";
+                        btnSave.Enabled = false;
+                        tbEdit.ReadOnly = true;
+                    }
+                }
+            } catch (IOException ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.ToString());
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                fsSource = new FileStream(fileName, FileMode.Truncate, FileAccess.Write);
+                swSource = new   StreamWriter(fsSource);
+                for (int i = 0; i < tbEdit.Lines.Length; i++)
+                { 
+                    swSource.WriteLine(tbEdit.Lines[i]); 
+                
+                } swSource.Close();
+                MessageBox.Show("Изменения сохранены!" );
+            }
+            catch ( IOException ex)
+            { 
+                MessageBox.Show("Произошла ошибка: "   + ex.ToString());
+            }
+            catch ( UnauthorizedAccessException ex)
+            { 
+                MessageBox.Show("Произошла ошибка доступа: "   + ex.ToString());
+            }
+        }
+    }
+}
